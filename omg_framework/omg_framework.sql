d520c94a5010aef1902d4df965e0bacea6482473
Rubylium <alexisoko0@gmail.com>
2020-05-03 17:19:34 +0200

    Fixed SQL

--- omg_framework/omg_framework.sql
+++ omg_framework/omg_framework.sql
@@ -8,14 +8,14 @@ CREATE TABLE `player_account` (
   `player_first_spawn` varchar(255) NOT NULL DEFAULT 'first_spawn',
   `player_identifier` varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
   `player_health` varchar(255) NOT NULL DEFAULT '200', /* WIP*/
-  `player_position` text NOT NULL,
+  `player_position` text DEFAULT NULL,
   `player_group` varchar(50) NOT NULL DEFAULT '0', /* WIP*/
   `player_permission_level` int(11) NOT NULL DEFAULT '0', /* WIP */
   `player_money` double NOT NULL DEFAULT '0',
   `player_bank_balance` int(32) DEFAULT '0',
   `player_dirty_money` double NOT NULL DEFAULT '0',
   `player_job` varchar(50) NOT NULL DEFAULT 'police', /* WIP*/
-  `player_inv` text DEFAULT '{}', /* WIP*/
+  `player_inv` text DEFAULT NULL /* WIP*/
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 -- --------------------------------------------------------
